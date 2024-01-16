9a0f3f06c43ac2dd4a45415bf819a356ffcfb1b6
VaibhavAPatil <Vaibhavpatil.vap@gmail.com>
2024-01-16 12:58:56 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//app.use() is used for configuration and middleware
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+// url encoding⬇️
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// Asset Management public folder⬇️
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path:'./env'
 })
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server Is Running at port: ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MONGO DB Connection failed !!!", err);
+})
 
 /*
 import express from "express";
